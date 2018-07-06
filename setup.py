891b3adb2dc35b863ffbed7a705c91929c90ea45
Mansoor Alam <mansooralam@users.noreply.github.com>
2018-07-05 21:41:50 -0400

    update url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pyCCDA',
       version='0.0.1',
       description='CCDA Parser',
-      url='http://github.com/mansooralam/pyCCDA',
+      url='https://github.com/MemoirHealth/ccda-parser',
       author='Mansoor Alam',
       author_email='me@mansooralam.com',
       license='MIT',
